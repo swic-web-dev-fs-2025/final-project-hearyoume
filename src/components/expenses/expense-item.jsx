8c48716cc1d48849d5565509f3f0627e2b3d713a
Amanda Ellis <amanda.ellis002@student.swic.edu>
2025-12-05 17:04:49 -0600

    Add delete button functionality to ExpenseItem component

--- src/components/expenses/expense-item.jsx
+++ src/components/expenses/expense-item.jsx
@@ -1,8 +1,15 @@
-export default function ExpenseItem({ expense }) {
+export default function ExpenseItem({ expense, handleDeleteExpense }) {
   // Renders individual expense details
   return (
     <li>
       {expense.place}: ${expense.amount} ({expense.recurring})
+      {/* Delete button */}
+      <button
+        onClick={() => handleDeleteExpense(expense.id)}
+        className="ml-4 px-2 py-1 bg-red-500 text-white rounded hover:bg-red-600"
+      >
+        Delete
+      </button>
     </li>
   );
 }
